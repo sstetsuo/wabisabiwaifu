bbd6412747f7c8cff0422c7fe74ad23b6a72db2a
sstetsuo <sneakisenpai@gmail.com>
2020-03-13 17:26:40 -0700

    Initial setup - React/Express/Webpack/Babel

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 5000;
+
+app.listen(port, () => {
+  console.log(`Server is running, listening on port ${port}`);
+});
+
+app.use(express.static(path.join(__dirname, '../client/dist')));

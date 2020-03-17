38c459a649b866907d55867ca5c2943f9bb392b7
sstetsuo <sneakisenpai@gmail.com>
2020-03-17 08:31:38 -0700

    Add db connection check

--- client/src/components/card.jsx
+++ client/src/components/card.jsx
@@ -1,17 +1,17 @@
 /* eslint-disable no-console */
-import React, { Component } from 'react';
-import $ from 'jquery';
+import React, { Component } from "react";
+import $ from "jquery";
 
 class Card extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      waifu: {},
+      waifu: {}
     };
   }
 
   componentDidMount() {
-    $.get('/api/testWaifu', (data) => {
+    $.get("/api/testWaifu", data => {
       console.log(data[0]);
       this.setState({ waifu: data[0] });
     });
@@ -23,21 +23,19 @@ class Card extends Component {
     return (
       <div className="card text-center">
         <div className="card-body text-dark">
-          <h4 className="card-title">
-            {waifu.name}
-          </h4>
-          <p className="card-text text-secondary">
-            {`Age: ${waifu.age}, Height ${waifu.height} cm, Weight: ${waifu.weight} kg`}
-          </p>
-          <p className="card-text text-secondary">
-            {`Skintone: ${waifu.skintone}, Cupsize ${waifu.cupsize}`}
-          </p>
-          <p className="card-text text-secondary">
-            {`Her hair is ${waifu.hairColor}, worn with ${waifu.hairstyle}.`}
-          </p>
-          <p className="card-text text-secondary">
-            {`She is a ${waifu.disposition} ${waifu.attribute} and ${waifu.vice}.`}
-          </p>
+          <h4 className="card-title">{waifu.name}</h4>
+          <div className="card-text text-secondary">
+            <p>
+              {`Age: ${waifu.age}, Height ${waifu.height} cm, Weight: ${waifu.weight} kg`}
+            </p>
+            <p>{`Skintone: ${waifu.skintone}, Cupsize ${waifu.cupsize}`}</p>
+            <p>
+              {`Her hair is ${waifu.hairColor}, worn with ${waifu.hairstyle}.`}
+            </p>
+            <p>
+              {`She is a ${waifu.disposition} ${waifu.attribute} and ${waifu.vice}.`}
+            </p>
+          </div>
           {/* <a href="#" className="btn btn-outline-success">TEST</a> */}
         </div>
       </div>
--- database/index.js
+++ database/index.js
@@ -3,6 +3,12 @@ const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost:27017/wftodo', { useNewUrlParser: true, useUnifiedTopology: true });
 
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB:wftodo...');
+});
+
 const waifuSchema = mongoose.Schema({
   name: String,
   age: Number,

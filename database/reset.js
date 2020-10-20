917ded0f6f26aaed782efeb7d962acad81f9e345
sstetsuo <sneakisenpai@gmail.com>
2020-10-20 11:26:15 -0700

    Remove db files and scripts

--- database/reset.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-console */
-const mongoose = require('mongoose');
-const { Waifu } = require('./index.js');
-
-Waifu.deleteMany({})
-  .then(() => {
-    console.log('Database has been emptied of all waifus');
-    mongoose.connection.close();
-  });

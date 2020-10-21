1d3d44fb2528a976d857256ce6ea9dbacf8e307d
sstetsuo <sneakisenpai@gmail.com>
2020-10-21 06:28:59 -0700

    Cleanup server index

--- server/index.js
+++ server/index.js
@@ -1,27 +1,17 @@
-/* eslint-disable no-console */
 const express = require('express');
 const path = require('path');
 const Waifu = require('./models/Waifu');
-// serverside randomization only
-// const { randomWaifu } = require('../database/seed.js');
-// const { findWaifus } = require('../database/index.js');
 
 const app = express();
 const port = 5000;
 
-app.listen(port, () => {
-  console.log(`Server is running, listening on port ${port}`);
-});
-
 app.use(express.static(path.join(__dirname, '../client/dist')));
-
-// serverside randomization only
-// app.get('/api/testWaifu', (req, res) => {
-//   const data = randomWaifu(1);
-//   res.send(data);
-// });
 
 app.get('/api/loadWaifu', (req, res) => {
   const randomWaifu = new Waifu();
   res.send(randomWaifu);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running, listening on port ${port}`);
 });

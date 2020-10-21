439c1bc161dd72ff951717a1e9330fcb3d60a757
sstetsuo <sneakisenpai@gmail.com>
2020-10-21 12:15:11 -0700

    Typo

--- client/src/components/addCard.jsx
+++ client/src/components/addCard.jsx
@@ -7,7 +7,7 @@ const AddCard = ({ handleAdd }) => (
       What is Wabi Sabi?
     </h3>
     <p className="italics">
-    "Pared down to its barest essence, wabi-sabi is the Japanese art of finding beauty in imperfection and profundity in nature... It celebrates cracks and crevices and all the other marks that time, weather, and loving use leave behind. It reminds us that we are all but transient beings on this planet-that our bodies as well as the material world around us are in the process of returning to the dust from which we came. Through wabi-sabi, we learn to embrace liver spots, rust, and frayed edges, and the march of time they represent."
+    "Pared down to its barest essence, wabi-sabi is the Japanese art of finding beauty in imperfection and profundity in nature... it celebrates cracks and crevices and all the other marks that time, weather, and loving use leave behind. It reminds us that we are all but transient beings on this planet-that our bodies as well as the material world around us are in the process of returning to the dust from which we came. Through wabi-sabi, we learn to embrace liver spots, rust, and frayed edges, and the march of time they represent."
     <br />
     <a href="https://nobleharbor.com/tea/chado/WhatIsWabi-Sabi.htm">Sauce.</a>
     <br />
